efd67f4778d223577db77dc1d6c4f24e14c7c2ba
Pucwinmc <pucwinmc@gmail.com>
2026-03-05 03:50:08 +0700

    Update StackEngineX.java

--- src/main/java/me/phuc/stackenginex/StackEngineX.java
+++ src/main/java/me/phuc/stackenginex/StackEngineX.java
@@ -1,39 +1,21 @@
 package me.phuc.stackenginex;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StackEngineX extends JavaPlugin {
 
-    private int maxStack;
-
     @Override
     public void onEnable() {
         saveDefaultConfig();
-        loadPlugin();
-        getLogger().info("StackEngineX enabled!");
-    }
 
-    private void loadPlugin() {
-        reloadConfig();
-        maxStack = getConfig().getInt("max-stack-size", 128);
+        // Đăng ký sự kiện auto stack khi nhặt item
+        getServer().getPluginManager().registerEvents(new StackListener(this), this);
+
+        getLogger().info("StackEngineX đã bật!");
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        if (!command.getName().equalsIgnoreCase("stackenginex")) {
-            return false;
-        }
-
-        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            loadPlugin();
-            sender.sendMessage("§aStackEngineX config reloaded!");
-            return true;
-        }
-
-        sender.sendMessage("§eStackEngineX v1.0");
-        return true;
+    public void onDisable() {
+        getLogger().info("StackEngineX đã tắt!");
     }
 }
