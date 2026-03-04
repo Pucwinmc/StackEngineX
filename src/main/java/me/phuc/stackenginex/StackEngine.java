a8740c212dc74e68cf63e5481ec2b63fc89eadfb
Pucwinmc <pucwinmc@gmail.com>
2026-03-05 06:39:40 +0700

    mvn clean package

--- src/main/java/me/phuc/stackenginex/StackEngine.java
+++ src/main/java/me/phuc/stackenginex/StackEngine.java
@@ -1,1 +1,24 @@
+package me.phuc.stackenginex;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StackEngine extends JavaPlugin {
+
+    private static StackEngine instance;
+
+    public static StackEngine get() {
+        return instance;
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        getLogger().info("StackEngineX Enabled.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("StackEngineX Disabled.");
+    }
+}
