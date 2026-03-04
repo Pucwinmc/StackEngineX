2ae2569e18691e3462945fcc72f988b72daaeeca
Pucwinmc <pucwinmc@gmail.com>
2026-03-05 03:08:18 +0700

    Update StackEngineX.java

--- src/main/java/me/phuc/stackenginex/StackEngineX.java
+++ src/main/java/me/phuc/stackenginex/StackEngineX.java
@@ -1,16 +1,54 @@
 package me.phuc.stackenginex;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StackEngineX extends JavaPlugin {
 
+    private int maxStack;
+
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        loadPlugin();
         getLogger().info("StackEngineX enabled!");
     }
 
+    private void loadPlugin() {
+        reloadConfig();
+        maxStack = getConfig().getInt("max-stack-size");
+
+        Bukkit.getWorlds().forEach(world ->
+                world.setMaxStackSize(maxStack)
+        );
+    }
+
     @Override
-    public void onDisable() {
-        getLogger().info("StackEngineX disabled!");
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!command.getName().equalsIgnoreCase("stackenginex")) {
+            return false;
+        }
+
+        if (!sender.hasPermission("stackenginex.reload")) {
+            sender.sendMessage(color(getConfig().getString("messages.no-permission")));
+            return true;
+        }
+
+        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+            loadPlugin();
+            sender.sendMessage(color(getConfig().getString("messages.reload-success")));
+            return true;
+        }
+
+        sender.sendMessage(ChatColor.YELLOW + "/stackenginex reload");
+        return true;
+    }
+
+    private String color(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
     }
 }
