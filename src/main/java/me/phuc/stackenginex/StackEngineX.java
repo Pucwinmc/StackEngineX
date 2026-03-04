8a8519e345499b2d657066c0e4d0e3bcb7c498c4
Pucwinmc <pucwinmc@gmail.com>
2026-03-05 05:53:25 +0700

    Update StackEngineX.java

--- src/main/java/me/phuc/stackenginex/StackEngineX.java
+++ src/main/java/me/phuc/stackenginex/StackEngineX.java
@@ -1,33 +1,23 @@
 package me.phuc.stackenginex;
 
-import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StackEngineX extends JavaPlugin {
 
     private static StackEngineX instance;
-    private NamespacedKey storedKey;
 
     @Override
     public void onEnable() {
         instance = this;
 
-        storedKey = new NamespacedKey(this, "stored");
+        saveDefaultConfig();
 
-        Bukkit.getPluginManager().registerEvents(
-                new StackListener(this),
-                this
-        );
+        SettingsManager.load(this);
 
-        getLogger().info("StackEngineX enabled.");
+        getLogger().info("StackEngineX enabled!");
     }
 
-    public static StackEngineX getInstance() {
+    public static StackEngineX get() {
         return instance;
-    }
-
-    public NamespacedKey getStoredKey() {
-        return storedKey;
     }
 }
